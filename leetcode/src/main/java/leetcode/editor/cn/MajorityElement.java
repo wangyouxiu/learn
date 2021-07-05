e681f8ab85139bf9b3eb2f098d5780eb42a6df8c
WangYue <y.wang39@trip.com>
2021-07-05 19:16:35 +0800

    fix: hot easy

--- leetcode/src/main/java/leetcode/editor/cn/MajorityElement.java
+++ leetcode/src/main/java/leetcode/editor/cn/MajorityElement.java
@@ -30,7 +30,10 @@
   
 package leetcode.editor.cn;
 
+    import com.sun.jmx.snmp.SnmpUnknownMsgProcModelException;
+
     import java.util.Arrays;
+    import java.util.Random;
 
     public class MajorityElement{
     public static void main(String[] args) {
@@ -42,19 +45,78 @@ package leetcode.editor.cn;
 
 
         //排序，之后取中间的数，如果是偶数，取偏左或者偏右都可以。
-        public int majorityElement(int[] nums) {
-            Arrays.sort(nums);
-            return nums[nums.length / 2];
-        }
+//        public int majorityElement(int[] nums) {
+//            Arrays.sort(nums);
+//            return nums[nums.length / 2];
+//        }
 
 
         //随机化，因为众数占了n/2以上个位置，因此随机找一个数，判断它是否是众数，如果不是，再随机找下一个，直到找到为止。
+ /*       public int majorityElement(int[] nums) {
+            Random random = new Random();
+
+            int length = nums.length;
+
+            while (true) {
+                int i = randomInt(random, 0, length);
+                if (count(nums, nums[i]) > length / 2) {
+                    return nums[i];
+                }
+            }
+
+
+        }
+
+        //统计出现次数
+        public int count(int[] nums, int n) {
+            int count = 0;
+            for (int num : nums) {
+                if (num == n) {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        //计算一个随机值
+        public int randomInt(Random random, int min, int max) {
+            return random.nextInt(max - min) + min;
+        }*/
 
 
         //分治思想
         //如果一个数n是数组nums的众数，那么将nums分割为两部分后。n至少是某一部分的众数。因此采用分治思想， 将nums数组层层拆分
         //直到剩下一个数，那么这个数就是众数
         //再考虑合并，如果左半边的众数和右半边的众数相等，则合并后众数不变。如果二者不相等，则分别计算两个众数出现的次数，次数多的那个就是合并后的众数。
+        public int majorityElement(int[] nums) {
+            return majority(nums, 0, nums.length - 1);
+        }
+
+        public int majority(int[] nums, int l, int r) {
+            if (l == r) {
+                return nums[l];
+            }
+
+            int mid = l + ((r - l) >> 1);
+            int left = majority(nums, l, mid);
+            int right = majority(nums, mid + 1, r);
+
+            if (left == right) {
+                return left;
+            }
+
+            return count(nums, l, mid, left) > count(nums, mid + 1, r, right) ? left : right;
+        }
+
+        public int count(int[] nums, int l, int r, int n) {
+            int count = 0;
+            for (int i = l; i <= r; i++) {
+                if (nums[i] == n) {
+                    count++;
+                }
+            }
+            return count;
+        }
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
