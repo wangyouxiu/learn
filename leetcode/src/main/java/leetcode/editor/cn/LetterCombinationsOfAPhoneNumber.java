894917a5ecdbcba68ba9650c119df1089ade1415
WangYue <y.wang39@trip.com>
2021-07-06 17:38:51 +0800

    fix: 17.回溯算法

---
+++ leetcode/src/main/java/leetcode/editor/cn/LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,110 @@
+//给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+//
+// 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。 
+//
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：digits = "23"
+//输出：["ad","ae","af","bd","be","bf","cd","ce","cf"]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：digits = ""
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：digits = "2"
+//输出：["a","b","c"]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= digits.length <= 4 
+// digits[i] 是范围 ['2', '9'] 的一个数字。 
+// 
+// Related Topics 哈希表 字符串 回溯 
+// 👍 1383 👎 0
+
+
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LetterCombinationsOfAPhoneNumber {
+    public static void main(String[] args) {
+        Solution solution = new LetterCombinationsOfAPhoneNumber().new Solution();
+
+        solution.letterCombinations("23");
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        /**
+         * 回溯算法框架
+         * result = []
+         * def backtrack(路径, 选择列表):
+         *     if 满足结束条件:
+         *         result.add(路径)
+         *         return
+         *
+         *     for 选择 in 选择列表:
+         *         做选择
+         *         backtrack(路径, 选择列表)
+         *         撤销选择
+         * @param digits
+         * @return
+         */
+
+        public List<String> letterCombinations(String digits) {
+            List<String> combinations = new ArrayList<String>();
+            if (digits.length() == 0) {
+                return combinations;
+            }
+            Map<Character, String> phoneMap = new HashMap<Character, String>() {{
+                put('2', "abc");
+                put('3', "def");
+                put('4', "ghi");
+                put('5', "jkl");
+                put('6', "mno");
+                put('7', "pqrs");
+                put('8', "tuv");
+                put('9', "wxyz");
+            }};
+            backtrack(combinations, phoneMap, digits, 0, new StringBuffer());
+            return combinations;
+        }
+
+        public void backtrack(List<String> combinations, Map<Character, String> phoneMap, String digits, int index, StringBuffer combination) {
+            if (index == digits.length()) {
+                combinations.add(combination.toString());
+            } else {
+                char digit = digits.charAt(index);
+                String phone = phoneMap.get(digit);
+                for (int i = 0; i < phone.length(); i++) {
+                    combination.append(phone.charAt(i));
+                    backtrack(combinations, phoneMap, digits, index + 1, combination);
+                    combination.deleteCharAt(index);
+                }
+            }
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
