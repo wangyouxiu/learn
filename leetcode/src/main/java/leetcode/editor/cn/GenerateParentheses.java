bea54bf80734055dedf4dff79ab1506bd632abf0
WangYue <y.wang39@trip.com>
2021-07-07 11:20:18 +0800

    fix: 22.括号生成[回溯]

--- leetcode/src/main/java/leetcode/editor/cn/GenerateParentheses.java
+++ leetcode/src/main/java/leetcode/editor/cn/GenerateParentheses.java
@@ -41,15 +41,41 @@ public class GenerateParentheses {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
 
+        /**
+         * 回溯法
+         * 回溯法在根本上也是一种穷举，只是在穷举的过程中，只选择有可能的结果。因此比穷举效率高。
+         * 对于回溯法来讲 backtrack 方法本身可以完成某个位置之后的穷举。
+         * 而对于当前位置来讲，做出自己的选择，之后交给该方法。需要注意的是在 backtrack 完成之后，要清除自己的选择。
+         * @param n
+         * @return
+         */
         public List<String> generateParenthesis(int n) {
             List<String> result = new ArrayList<>();
-
+            backtrack(result, new StringBuilder(), 0, 0, n);
             return result;
+        }
+
+        public void backtrack(List<String> result, StringBuilder cur, int open, int close, int max) {
+            if (cur.length() == 2 * max) {
+                result.add(cur.toString());
+            }
+            if (open < max) {
+                cur.append('(');
+                backtrack(result, cur, open + 1, close, max);
+                cur.deleteCharAt(cur.length() -1);
+            }
+            if (close < open) {
+                cur.append(')');
+                backtrack(result, cur, open, close + 1, max);
+                cur.deleteCharAt(cur.length() - 1);
+            }
+
         }
 
 
         /**
          * 穷举，在穷举过程中校验是否否和要求，符合要求则加入结果集中，否则丢弃
+         * 穷举的核心是，在每个选择的节点上，都有'(' ')'两种选择。而穷举就是要走遍每一种选择
          * @param n
          * @return
          */
