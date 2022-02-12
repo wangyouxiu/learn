a0fcb75709a290722685b329ff82e5eecc25328b
wangYue <269773646@qq.com>
2022-02-12 14:50:34 +0800

    fix(+) : 每日一题

--- leetcode/src/main/java/leetcode/editor/cn/NumberOfEnclaves.java
+++ leetcode/src/main/java/leetcode/editor/cn/NumberOfEnclaves.java
@@ -38,6 +38,9 @@
 
 package leetcode.editor.cn;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class NumberOfEnclaves {
     public static void main(String[] args) {
         Solution solution = new NumberOfEnclaves().new Solution();
@@ -47,41 +50,109 @@ public class NumberOfEnclaves {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
 
-        int[] dx = {0, 1, 0, -1};
-        int[] dy = {1, 0, -1, 0};
+        // bfs 方法
+         int[] dx = {0,1,0,-1};
+         int[] dy = {1,0,-1,0};
 
-        boolean[][] flag;
-
+        boolean[][] visited;
+        int n , m;
+        Queue<int[]> queue = new ArrayDeque();
         public int numEnclaves(int[][] grid) {
-            int n = grid.length, m = grid[0].length, count = 0;
-            flag = new boolean[n][m];
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    if (grid[i][j] == 1) {
-                        count += dfs(i, j, n, m, grid);
+            n = grid.length;
+            m = grid[0].length;
+            visited = new boolean[n][m];
+
+            for(int i = 0 ; i< n ;i++){
+                if(grid[i][0] == 1){
+                    visited[i][0] = true;
+                    queue.offer(new int[]{i,0});
+                }
+                if(grid[i][m-1] == 1){
+                    visited[i][m-1] = true;
+                    queue.offer(new int[]{i,m-1});
+                }
+            }
+
+            for(int j = 1 ; j < m-1 ;j++){
+                if(grid[0][j] == 1){
+                    visited[0][j] = true;
+                    queue.offer(new int[]{0,j});
+                }
+                if(grid[n-1][j] == 1){
+                    visited[n-1][j] = true;
+                    queue.offer(new int[]{n-1,j});
+                }
+            }
+
+            while(!queue.isEmpty()){
+                int[] curr = queue.poll();
+                for(int k = 0 ; k < 4 ;k++){
+                    int tx = curr[0] + dx[k];
+                    int ty = curr[1] + dy[k];
+                    if(tx >= 0 && tx < n && ty >= 0 && ty < m && grid[tx][ty] == 1 && !visited[tx][ty]){
+                        visited[tx][ty] = true;
+                        queue.offer(new int[]{tx,ty});
                     }
                 }
             }
-            return count;
+
+            int ans = 0;
+            for(int i = 0 ; i < n ; i++){
+                for(int j = 0 ; j < m ; j++){
+                    if(grid[i][j] == 1 && !visited[i][j]){
+                        ans++;
+                    }
+                }
+            }
+            return ans;
         }
 
+        // dfs 方法
+        // static int[] dx = {0,1,0,-1};
+        // static int[] dy = {1,0,-1,0};
 
-        // 计算不能出去的1
-        public int dfs(int i, int j, int n, int m, int[][] grid) {
-            if (i < 0 || i >= n || j < 0 || j >= m || grid[i][j] == 2) {
-                return 0;
-            }
-            if (grid[i][j] == 0) {
-                return 1;
-            }
-            int count = 0;
-            for (int k = 0; k < 4; k++) {
-                int tx = i + dx[k];
-                int ty = j + dy[k];
-                count += dfs(tx, ty, n, m, grid);
-            }
-            return count == 4 ? 1 : 0;
-        }
+        // boolean[][] visited;
+        // int n , m;
+
+
+
+        // public int numEnclaves(int[][] grid) {
+        //     n = grid.length;
+        //     m = grid[0].length;
+        //     visited = new boolean[n][m];
+
+        //     for(int i = 0 ; i < n ; i++){
+        //         dfs(i,0,grid);
+        //         dfs(i,m-1,grid);
+        //     }
+
+        //     for(int j = 1 ; j < m-1 ; j++){
+        //         dfs(0,j,grid);
+        //         dfs(n-1,j,grid);
+        //     }
+
+        //     int count = 0;
+        //     for(int i = 0 ; i < n ; i++){
+        //         for(int j = 0 ; j < m ;j++){
+        //             if(grid[i][j] == 1 && !visited[i][j]){
+        //                 count++;
+        //             }
+        //         }
+        //     }
+        //     return count;
+        // }
+
+        // public void dfs(int i , int j , int[][] grid){
+        //     if(i < 0 || i >= n || j < 0 || j >= m || grid[i][j] == 0 || visited[i][j]){
+        //         return;
+        //     }
+
+        //     visited[i][j] = true;
+
+        //     for(int k = 0 ; k < 4 ;k++){
+        //         dfs( i+dx[k] , j+dy[k] , grid);
+        //     }
+        // }
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
